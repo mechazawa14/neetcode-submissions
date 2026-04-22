d39e0e4ba16668113d1dae18af48581509280824
MD. ABDUL RAHEEM ALAM <armay1405@gmail.com>
2026-04-22 09:46:20 +0530

    Add: is-anagram - submission-22

---
+++ Data Structures & Algorithms/is-anagram/submission-22.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t) :
+            return False 
+        # anagrams = []
+        for i in s :
+            if i not in t or s.count(i) != t.count(i):
+                return False 
+        return True 
+            
+            
