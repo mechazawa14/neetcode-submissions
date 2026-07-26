1f3112642934cb5906fa035865ee6150c7f2b68c
MD. ABDUL RAHEEM ALAM <armay1405@gmail.com>
2026-07-26 17:20:04 +0530

    Add: duplicate-integer - submission-29

---
+++ Data Structures & Algorithms/duplicate-integer/submission-29.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        sets = set()
+        for i in nums :
+            if i in sets:
+                return True 
+            else :
+                sets.add(i)
+        return False 
