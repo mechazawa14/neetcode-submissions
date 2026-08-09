3036d01ac7f37e1f69a9a9730ed01fb2a3bbba6c
MD. ABDUL RAHEEM ALAM <armay1405@gmail.com>
2026-08-09 20:23:29 +0530

    Add: is-palindrome - submission-12

---
+++ Data Structures & Algorithms/is-palindrome/submission-12.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        import re 
+        s = s.strip()
+        s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+
+        i = 0 
+        j = len(s)-1
+        while i <= j :
+            if s[i]!=s[j]:
+                return False
+            i+=1 
+            j-=1
+        return True
+
