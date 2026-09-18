52a9159debdff121875a1d77f18da3414f51cdb6
MD. ABDUL RAHEEM ALAM <armay1405@gmail.com>
2026-09-18 10:16:49 +0530

    Add: kth-largest-integer-in-a-stream - submission-6

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-6.py
@@ -0,0 +1,17 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        import heapq 
+        self.hoopa = nums 
+        self.thlargestweneed = k 
+        heapq.heapify(self.hoopa)
+        while len(self.hoopa) > self.thlargestweneed:
+            heapq.heappop(self.hoopa)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.hoopa, val)
+        if len(self.hoopa) > self.thlargestweneed:
+            heapq.heappop(self.hoopa)
+
+        return self.hoopa[0]
+
